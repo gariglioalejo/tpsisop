e09118a5db914923b0eb8bd485538b29be516624
gariglioalejo <gariglio.alejo@gmail.com>
2014-09-08 17:25:10 -0300

    Agregada convertirIntADir();

--- tpsisoplib/tpsisoplib.c
+++ tpsisoplib/tpsisoplib.c
@@ -7,6 +7,34 @@
 
 #include "tpsisoplib.h"
 
+//CONVERTIR UN INT32 A UNA ESTRUCTURA DE DIRECCIONES
+//USO t_msp_dir *direccion = convertirIntADir(1772423663);
+t_msp_dir * convertirIntADir(uint32_t dir){
+
+		t_msp_dir *msp_dir_aux = malloc(sizeof(t_msp_dir));
+
+		uint32_t aux = dir;
+
+		//printf("%d\n",aux);
+		//printf("%d\n",sizeof(aux));
+
+		int elevando1 = pow(2,8);
+		uint32_t aux1 = (aux % elevando1);
+		//printf("%d\n",aux1);
+		msp_dir_aux->offset = aux1;
+
+		int elevando2 = pow(2,20);
+		uint32_t aux2 = ((aux % elevando2) / elevando1);
+		//printf("%d\n",aux2);
+		msp_dir_aux->pagina = aux2;
+
+		uint32_t aux3 = (aux / elevando2);
+		//printf("%d\n",aux3);
+		msp_dir_aux->segmento = aux3;
+
+
+	return msp_dir_aux;
+}
 
 
 char * recibir_serializado(int socketCliente) {
--- tpsisoplib/tpsisoplib.h
+++ tpsisoplib/tpsisoplib.h
@@ -23,6 +23,8 @@
 #include <unistd.h>
 #include <commons/config.h>
 #include <pthread.h>
+#include <stdint.h>
+#include <math.h>
 
 typedef struct {
 	int pid;
@@ -40,9 +42,19 @@ typedef struct {
 	int E;
 } t_tid;
 
+typedef struct {
+
+	uint32_t segmento;
+	uint32_t pagina;
+	uint32_t offset;
+
+} t_msp_dir;
+
 
 char * recibir_serializado(int socketCliente);
 char * enviar_serializado(int codigo, char * mensaje, int socket);
 int conectarse(char * ip, char * puerto);
 int crearServer(char * puerto);
 int aceptarConexion(int listenningSocket);
+t_msp_dir * convertirIntADir(uint32_t dir);
+
