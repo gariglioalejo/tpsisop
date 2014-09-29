16263f772bb0d6349774da503c47e30cf06ecb68
martinBasile <sisopmartin@gmail.com>
2014-09-28 22:30:33 -0300

    Empezamos la MSP, creamos las estructuras auxiliares necesarias y la consola de la MSP. ademas comenzamos a armar la escucha de conexiones, ya sean de Kernel o CPUs

---
+++ MSP/src/MSP.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : MSP.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+void gestionarMemoria(uint32_t,uint32_t,uint32_t,char*);
+
+int main() {
+	/*void main(int argc, char *agrv){int puerto;int cantidadMemoria;int cantidadSwap;char* sustPags;
+if (argc != 2){printf("no ingreso suficientes argumentos");return};
+archivoConfiguracion=config_create(argv(2));
+puerto=get_int_value(archivoConfiguracion,"PUERTO");
+cantidadMemoria=get_int_value(archivoConfiguracion,"CANTIDAD_MEMORIA");
+cantidadSwap=get_int_value(archivoConfiguracion,"CANTIDAD_SWAP");
+sustPags=get_string_value(archivoConfiguracion,"SUST_PAGS");*/
+}
+ /* uint32_t puerto,cantMemoria,cantSwap;
+    char* algoritmo //puede ser LRU o Clock
+    leer archivo config
+	gestionarMemoria(puerto,cantMemoria,cantSwap,algoritmo);
+	return 0;
+}
+
+
+
