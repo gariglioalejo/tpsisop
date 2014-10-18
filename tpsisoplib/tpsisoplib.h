6ab86bb2a61746cf5e7a4475303b3d2fb66904c3
gariglioalejo <gariglio.alejo@gmail.com>
2014-10-18 17:32:09 -0300

    Update tpsisoplib.h

--- tpsisoplib/tpsisoplib.h
+++ tpsisoplib/tpsisoplib.h
@@ -4,6 +4,13 @@
  *  Created on: 26/08/2014
  *      Author: utnso
  */
+typedef struct {
+
+	uint32_t segmento;
+	uint32_t pagina;
+	uint32_t offset;
+
+} t_msp_dir;
 
 t_msp_dir * convertirIntADir(uint32_t dir);
 uint32_t convertirDirAInt(t_msp_dir * direccion);
