fd577cca8c77f7c2996e8b5d1eea22eba196a7b8
gariglioalejo <gariglio.alejo@gmail.com>
2014-11-28 02:24:34 -0300

    Panel de CPU

--- Cpu/cpu.h
+++ Cpu/cpu.h
@@ -8,11 +8,17 @@
 #ifndef CPU_H_
 #define CPU_H_
 
-
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/error.h>
 #include <commons/process.h>
+#include <commons/collections/list.h>
+#include <commons/string.h>
+//#include <panel/cpu.h>
+//#include <panel/panel.h>
+//#include <panel/kernel.h>
+
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -25,6 +31,7 @@
 #include <stdint.h>
 #include <math.h>
 
+
 //#include "../ensalada de funciones/funciones.h"
 #include "../tpsisoplib/tpsisoplib.h"
 
@@ -34,20 +41,77 @@ int socketK;
 int socketM;
 int segmentatioFault;
 
+
+
+//===INICIO PANEL
+typedef enum {
+	KERNEL, CPU
+} t_tipo_proceso;
+typedef enum {
+	NEW, READY, EXEC, BLOCK, EXIT
+} t_cola;
+
+typedef struct {
+	uint32_t pid;
+	uint32_t tid;
+	bool kernel_mode;
+	uint32_t segmento_codigo;
+	uint32_t segmento_codigo_size;
+	uint32_t puntero_instruccion;
+	uint32_t base_stack;
+	uint32_t cursor_stack;
+	int32_t registros[5];
+	t_cola cola;
+} t_hilo;
+
+typedef struct {
+	int32_t registros_programacion[5]; //A, B, C, D y E
+	uint32_t M; //Base de segmento de código
+	uint32_t P; //Puntero de instrucción
+	uint32_t X; //Base del segmento de Stack
+	uint32_t S; //Cursor de stack
+	uint32_t K; //Kernel Mode
+	uint32_t I; //PID
+} t_registros_cpu;
+
+t_registros_cpu * hola;
+t_log* logp;
+t_list* parametros;
+
+
+void inicializar_panel(t_tipo_proceso tipo_proceso, char* path);
+void comienzo_ejecucion(t_hilo* hilo, uint32_t quantum);
+void fin_ejecucion();
+void ejecucion_instruccion(char* mnemonico, t_list* parametros);
+void cambio_registros(t_registros_cpu registros);
+void ejecucion_hilo(t_hilo* hilo, uint32_t quantum);
+
+
+
+//===PANEL AUX
+
+void copiarHiloPanel(t_hilo * hiloPanel, t_tcb * tcb);
+void copiarRegistrosPanel(t_registros_cpu * registrosPanel, t_tcb * tcb);
+void panel_instruccion(char** params, int cant, char* instruccion);
+
+//===FIN PANEL
+
+
+
 //Funciones Privilegiadas
-int fnMALC (t_tcb * tcb);
-int fnFREE (t_tcb * tcb);
-int fnINNN (t_tcb * tcb);
-int fnINNC (t_tcb * tcb);
-int fnOUTN (t_tcb * tcb);
-int fnOUTC (t_tcb * tcb);
-int fnCREA (t_tcb * tcb);
-int fnJOIN (t_tcb * tcb);
-int fnBLOK (t_tcb * tcb);
-int fnWAKE (t_tcb * tcb);
+int fnMALC(t_tcb * tcb);
+int fnFREE(t_tcb * tcb);
+int fnINNN(t_tcb * tcb);
+int fnINNC(t_tcb * tcb);
+int fnOUTN(t_tcb * tcb);
+int fnOUTC(t_tcb * tcb);
+int fnCREA(t_tcb * tcb);
+int fnJOIN(t_tcb * tcb);
+int fnBLOK(t_tcb * tcb);
+int fnWAKE(t_tcb * tcb);
 
 //Parser
-int parseador(char*,t_tcb*);
+int parseador(char*, t_tcb*);
 
 //FuncionesNoPrivilegiadas
 int load(t_tcb*);
